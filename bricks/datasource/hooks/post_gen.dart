ec9797b000566cb0133262079a45bc567e204b64
Vinicius Amélio <vinicius.amelio@zoho.com>
2023-11-13 21:43:12 -0300

    replacing cli spin by slug

--- bricks/datasource/hooks/post_gen.dart
+++ bricks/datasource/hooks/post_gen.dart
@@ -1,30 +1,36 @@
 import 'dart:io';
 
-import 'package:cli_spin/cli_spin.dart';
 import 'package:mason/mason.dart';
+import 'package:slug/slug.dart';
 
 void run(HookContext context) async {
-  final uuidSpinner = CliSpin(
-    text: "Installing dependencies",
-    spinner: CliSpinners.arc,
-  );
-  uuidSpinner.info();
-  await Process.run("flutter", ["pub", "add", "uuid"]);
-  uuidSpinner.success("UUID installed ✅✅");
-  if (!context.vars["couchbase"]) {
-    uuidSpinner.success("Couchbase installation skipped ⏩⏩");
-    await Process.run(
-      "rm",
-      ["-R", "lib/core/database/couch_database"],
+  final hookSlug = Slug(slugStyle: SlugStyle.toggle7);
+  final hookProgress = hookSlug.progress("Running installation script");
+  try {
+    final uuidSlug = Slug(slugStyle: SlugStyle.toggle7);
+    final uuidProgress = uuidSlug.progress("Installing UUID package");
+    await Process.run("flutter", ["pub", "add", "uuid"]);
+    uuidProgress.finish(message: "UUID installed ✅✅");
+    if (!context.vars["couchbase"]) {
+      uuidProgress.finish(message: "Couchbase installation skipped ⏩⏩");
+      await Process.run(
+        "rm",
+        ["-R", "lib/core/database/couch_database"],
+        runInShell: true,
+      );
+      return;
+    }
+    final couchbaseSlug = Slug(slugStyle: SlugStyle.toggle7);
+    final couchbaseProgress =
+        couchbaseSlug.progress("Installing Couchbase Lite package");
+    await Process.runSync(
+      "flutter",
+      ["pub", "add", "cbl_flutter", "cbl", "cbl_flutter_ce"],
       runInShell: true,
     );
-    return;
+    couchbaseProgress.finish(message: "Couchbase Lite installed ✅✅");
+    hookProgress.finish();
+  } catch (e) {
+    hookProgress.error();
   }
-  uuidSpinner.info("Installing couchbase packages");
-  await Process.runSync(
-    "flutter",
-    ["pub", "add", "cbl_flutter", "cbl", "cbl_flutter_ce"],
-    runInShell: true,
-  );
-  uuidSpinner.success("Couchbase Lite installed ✅✅");
 }
